c2494844ed2de4b9897bff9a3ea85a2c2ed1908d
Fidha Rafeeque <fidharafeequen@gmail.com>
2023-10-05 20:48:07 +0400

    homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,24 @@
+
 import './App.css';
+import React from 'react'
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/layouts/Header';
 import Footer from './components/layouts/Footer';
-
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="">
+    <Router>
       <Header />
+      <div className="container container-fluid">
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
